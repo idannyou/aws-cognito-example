6b57f1abd1abed23d28f55f35e91bda61500ad5e
Danny Ou <danny.ou@procore.com>
2018-08-22 21:04:45 -0700

    Add Sign In and Sign Up form for Cognito

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { Box, Button, Flex, Input, Link } from '@procore/core-react';
+
 class App extends Component {
+  state = {
+    confirmationCode: '',
+    userName: '',
+    password: '',
+    signInMode: true
+  };
+
+  onChangeText = state => event => {
+    this.setState({
+      [state]: event.target.value
+    });
+  };
+
+  toggleMode = () => {
+    this.setState({
+      signInMode: !this.state.signInMode
+    });
+  };
+
+  triggerAction = action => () => {
+    const { confirmationCode, userName, password } = this.state;
+
+    console.log(`triggered ${action}`, confirmationCode, userName, password);
+  };
+
   render() {
+    const { confirmationCode, userName, password, signInMode } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        direction="column"
+        style={{ width: '100%' }}
+      >
+        <Flex alignContent="space-between">
+          <Box margin="sm">
+            <Link onClick={this.toggleMode}>
+              {signInMode ? 'Sign Up' : 'Sign In'}
+            </Link>
+          </Box>
+        </Flex>
+
+        <Box margin="lg">
+          <Input
+            placeholder="User Name"
+            value={userName}
+            onChange={this.onChangeText('userName')}
+          />
+        </Box>
+        <Box margin="lg">
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={this.onChangeText('password')}
+          />
+        </Box>
+        <Button onClick={this.triggerAction('Sign')}>
+          {signInMode ? 'Sign In' : 'Sign Up'}
+        </Button>
+        {!signInMode && (
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+            style={{ width: '100%' }}
+          >
+            <Box margin="lg">
+              <Input
+                placeholder="Verification Code"
+                value={confirmationCode}
+                onChange={this.onChangeText('confirmationCode')}
+              />
+            </Box>
+            <Button onClick={this.triggerAction('Confirm Code')}>
+              {'Confirm Code'}
+            </Button>
+          </Flex>
+        )}
+      </Flex>
     );
   }
 }
